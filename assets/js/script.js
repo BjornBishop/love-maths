2e6f024ca0ae972e05ade9eb7cce5f91afda9808
Bjorn Bishop <bjornbishop@gmail.com>
2024-06-23 21:06:02 +0000

    added multiple function

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,10 @@ function runGame(gameType) {
     let num2 = Math.floor(Math.random() * 25) +1;
 
     if (gameType === "addition") {
-        displayAdditionQuestion(num1, num2) 
+        displayAdditionQuestion(num1, num2);
+    } else if (gameTpye === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    
     } else {
         alert(`unknown game type: ${gameType}`);
         throw `Unkown game type: ${gameType}. Aborting!`;
@@ -71,7 +74,9 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    } else {
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+    }else {
         alert(`Unimplemented Operator ${operator}`);
         throw `Unimplemented Operator ${operator}. Aborting!`;
     }
@@ -105,7 +110,11 @@ function displaySubtractionQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";   
 
 }
 

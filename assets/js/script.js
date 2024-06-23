335fd9a4726d2f827a1b74bb95dbf294e5b8e375
Bjorn Bishop <bjornbishop@gmail.com>
2024-06-23 20:52:29 +0000

    incorrect and correct score count added

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,8 +48,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`awww... You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -75,12 +77,20 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * gets the current score from the DOM and adds 1
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * gets the current incorrect answers and adds 1 
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
